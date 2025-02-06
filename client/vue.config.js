15f75d97b43b6b710266be840018cd555bc1f9a9
MarkDanilchenko <danilchenko.mark50@gmail.com>
2025-02-06 22:22:19 +0300

    Improve vue settings

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,8 +1,12 @@
-const { defineConfig } = require('@vue/cli-service');
-const Dotenv = require('dotenv-webpack');
+const { defineConfig } = require("@vue/cli-service");
+const Dotenv = require("dotenv-webpack");
+
 module.exports = defineConfig({
-	transpileDependencies: true,
-	configureWebpack: {
-		plugins: [new Dotenv({ path: '../.env' })],
-	},
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new Dotenv({ path: "../.env.development" }),
+      // new Dotenv({ path: "../.env.public" }),
+    ],
+  },
 });
