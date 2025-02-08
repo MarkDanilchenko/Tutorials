3ec3c05c4a8b4dff75921d0176a33c94484a2b4d
MarkDanilchenko <danilchenko.mark50@gmail.com>
2025-02-08 18:03:55 +0300

    Code refactoring

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,61 +1,66 @@
 <template>
-    <div class="container">
-        <Navbar />
-        <router-view v-slot="{ Component }">
-            <transition name="fade" mode="out-in">
-                <component :is="Component" />
-            </transition>
-        </router-view>
-        <Footer />
-    </div>
+  <div class="container">
+    <Navbar />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+    <Bottom />
+  </div>
 </template>
 
 <script>
-import eventBus from '@/services/eventBus.js';
-import { mapActions, mapGetters, mapMutations, mapState } from 'vuex';
-import Navbar from '@/components/Navbar.vue';
-import Footer from '@/components/Footer.vue';
+import eventBus from "@/services/eventBus.js";
+import Navbar from "@/components/Navbar.vue";
+import Bottom from "@/components/Bottom.vue";
+import { mapActions } from "vuex";
+
 export default {
-    name: 'App',
-    components: {
-        Navbar,
-        Footer
-    },
-    mounted() {
-        eventBus.on('authError', () => {
-            this.signOut_();
-        });
-        if (localStorage.getItem('refreshToken')) {
-            this.userProfile();
-        }
-    },
-    beforeDestroy() {
-        eventBus.remove('authError');
-    },
-    methods: {
-        ...mapActions({
-            signOut: 'auth/signOut',
-            userProfile: 'auth/userProfile',
-        }),
-        signOut_() {
-            if (localStorage.getItem('refreshToken')) {
-                this.signOut({ "refresh": localStorage.getItem('refreshToken') })
-            } else {
-                this.$router.push('/signin');
-            }
-        }
+  name: "App",
+  components: {
+    Navbar,
+    Bottom,
+  },
+  mounted() {
+    let refresh = localStorage.getItem("refreshToken");
+
+    if (refresh) {
+      this.userProfile();
     }
-}
+
+    eventBus.on("authCheck", () => {
+      refresh = localStorage.getItem("refreshToken");
+      this.authCheck(refresh);
+    });
+  },
+  beforeUnmount() {
+    eventBus.remove("authCheck");
+  },
+  methods: {
+    ...mapActions({
+      signOut: "auth/signOut", // TODO rename this method in vuex
+      userProfile: "auth/userProfile",
+    }),
+    authCheck(refresh) {
+      if (refresh) {
+        return this.signOut({ refresh });
+      }
+
+      this.$router.push("/signin");
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
 .fade-enter-active,
 .fade-leave-active {
-    transition: opacity 0.5s ease;
+  transition: opacity 0.5s ease;
 }
 
 .fade-enter-from,
 .fade-leave-to {
-    opacity: 0;
+  opacity: 0;
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,18 +1,18 @@
-import { createApp } from 'vue';
-import App from '@/App.vue';
-import router from '@/router/index.js';
-import store from '@/store/index.js';
-// import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap 5 is imported in client/src/assets/scss/index.scss
-import '@/assets/scss/index.scss';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import '@/assets/js/index.js';
-import directives_custom from '@/directives/index.js';
-window.$ = window.jQuery = require('jquery');
+import { createApp } from "vue";
+import App from "@/App.vue";
+import router from "@/router/index.js";
+import store from "@/store/index.js";
+import "@/assets/scss/index.scss";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@/assets/js/index.js";
+import directives_custom from "@/directives/index.js";
+
+window.$ = window.jQuery = require("jquery");
 
 const app = createApp(App);
 
 directives_custom.forEach((directive) => {
-	app.directive(directive.name, directive);
+  app.directive(directive.name, directive);
 });
 
-app.use(router).use(store).mount('#app');
+app.use(router).use(store).mount("#app");
