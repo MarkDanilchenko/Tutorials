9ce7100e7977be3c1737f2eaef008307399a3ba4
MarkDanilchenko <danilchenko.mark50@gmail.com>
2025-02-08 22:41:53 +0300

    When runserver - host and port from env

--- API/manage.py
+++ API/manage.py
@@ -7,6 +7,10 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tutorials.settings")
     try:
         from django.core.management import execute_from_command_line
+        from django.core.management.commands.runserver import Command as runserver
+
+        runserver.default_port = os.getenv("DJANGO_SERVER_PORT", "8000")
+        runserver.default_addr = os.getenv("DJANGO_SERVER_HOST", "127.0.0.1")
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
