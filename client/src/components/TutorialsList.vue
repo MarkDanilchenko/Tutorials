1d0b9395a04ada4f4aea6d067b08667a68bedcad
MarkDanilchenko <danilchenko.mark50@gmail.com>
2025-02-16 18:53:14 +0300

    Code refactoring in tutorilas list and direct item components

---
+++ client/src/components/TutorialsList.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="my-tutorialsList">
+    <div id="TutorialsAccordionPanel" class="accordion">
+      <TutorialItem
+        v-for="(tutorial, index) in tutorials"
+        :key="tutorial.id"
+        :tutorial="tutorial"
+        :index="index"
+        @tutorial-details="$emit('tutorial-details', $event)"
+      />
+    </div>
+  </section>
+</template>
+
+<script>
+import TutorialItem from "@/components/TutorialItem.vue";
+
+export default {
+  name: "TutorialsList",
+  components: {
+    TutorialItem,
+  },
+  props: {
+    tutorials: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  emits: ["tutorial-details"],
+};
+</script>
+
+<style scoped lang="scss"></style>
